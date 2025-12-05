7186af70b6e88f5b2869674eb04b6de5b08949ef
Gustavo Ishizava <gustavoishizava@hotmail.com>
2025-12-05 07:04:08 -0300

    Add QueueProperties unit tests

---
+++ src/AngelMQ.UnitTests/Properties/QueuePropertiesTests.cs
@@ -0,0 +1,25 @@
+using AngelMQ.Properties;
+
+namespace AngelMQ.UnitTests.Properties;
+
+public class QueuePropertiesTests
+{
+    [Theory]
+    [InlineData("orders", "orders-exchange")]
+    [InlineData("accounts", "accounts-exchange")]
+    public void Properties_ShouldReturnCorrectNamesWithSuffixes(string queueName, string exchangeName)
+    {
+        // Arrange
+        var properties = new QueueProperties<object>
+        {
+            QueueName = queueName,
+            ExchangeName = exchangeName
+        };
+
+        // Act & Assert
+        Assert.Equal($"{queueName}.dlq", properties.DeadLetterQueueName);
+        Assert.Equal($"{exchangeName}.dlx", properties.DeadLetterExchangeName);
+        Assert.Equal($"{queueName}.plq", properties.ParkingLotQueueName);
+        Assert.Equal($"{exchangeName}.plx", properties.ParkingLotExchangeName);
+    }
+}
