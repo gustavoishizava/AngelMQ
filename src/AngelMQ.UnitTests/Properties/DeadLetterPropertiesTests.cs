4f0a7b50c8acf3120716a4d2496016407bfd54b9
Gustavo Ishizava <gustavoishizava@hotmail.com>
2025-12-05 06:57:29 -0300

    DeadLetterProperties tests

---
+++ src/AngelMQ.UnitTests/Properties/DeadLetterPropertiesTests.cs
@@ -0,0 +1,70 @@
+using AngelMQ.Properties;
+
+namespace AngelMQ.UnitTests.Properties;
+
+public class DeadLetterPropertiesTests
+{
+    [Fact]
+    public void BuildQueueName_WithDefaultProperties_ShouldReturnMainQueueNameWithDefaultSuffix()
+    {
+        // Arrange
+        var properties = new DeadLetterProperties();
+        var mainQueueName = "orders";
+
+        // Act
+        var result = properties.BuildQueueName(mainQueueName);
+
+        // Assert
+        Assert.Equal("orders.dlq", result);
+    }
+
+    [Fact]
+    public void BuildQueueName_WithCustomQueueNameAndSuffix_ShouldReturnCustomQueueNameWithCustomSuffix()
+    {
+        // Arrange
+        var properties = new DeadLetterProperties
+        {
+            QueueName = "custom-queue",
+            QueueSuffix = ".custom"
+        };
+        var mainQueueName = "orders";
+
+        // Act
+        var result = properties.BuildQueueName(mainQueueName);
+
+        // Assert
+        Assert.Equal("custom-queue.custom", result);
+    }
+
+    [Fact]
+    public void BuildExchangeName_WithDefaultProperties_ShouldReturnMainExchangeNameWithDefaultSuffix()
+    {
+        // Arrange
+        var properties = new DeadLetterProperties();
+        var mainExchangeName = "orders-exchange";
+
+        // Act
+        var result = properties.BuildExchangeName(mainExchangeName);
+
+        // Assert
+        Assert.Equal("orders-exchange.dlx", result);
+    }
+
+    [Fact]
+    public void BuildExchangeName_WithCustomExchangeNameAndSuffix_ShouldReturnCustomExchangeNameWithCustomSuffix()
+    {
+        // Arrange
+        var properties = new DeadLetterProperties
+        {
+            ExchangeName = "custom-exchange",
+            ExchangeSuffix = ".custom"
+        };
+        var mainExchangeName = "orders-exchange";
+
+        // Act
+        var result = properties.BuildExchangeName(mainExchangeName);
+
+        // Assert
+        Assert.Equal("custom-exchange.custom", result);
+    }
+}
